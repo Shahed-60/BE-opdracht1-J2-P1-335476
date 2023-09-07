04380f62cb9bbd3b4218b1fbd8103c696d982001
Shahed Amer <chanyeol6521354@gmail.com>
2023-09-07 15:01:35 +0200

    mapje toegevoegd

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'Mvc-2209c-P4');
+define('URLROOT', 'http://BE-opdracht1-J2-P1-335476.com');
---
+++ app/models/InstructeurModel.php
@@ -0,0 +1,81 @@
+<?php
+
+class InstructeurModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getInstructeurs()
+    {
+        $sql = "SELECT Id
+                      ,Voornaam
+                      ,Tussenvoegsel
+                      ,Achternaam
+                      ,Mobiel
+                      ,DatumInDienst
+                      ,AantalSterren
+                FROM  Instructeur
+                ORDER BY AantalSterren DESC";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getToegewezenVoertuigen($Id)
+    {
+        $sql = "SELECT      
+                            VOER.Id
+                            ,VOER.Type
+                            ,VOER.Kenteken
+                            ,VOER.Bouwjaar
+                            ,VOER.Brandstof
+                            ,TYVO.TypeVoertuig
+                            ,TYVO.RijbewijsCategorie
+
+                FROM        Voertuig    AS  VOER
+                
+                INNER JOIN  TypeVoertuig AS TYVO
+
+                ON          TYVO.Id = VOER.TypeVoertuigId
+                
+                INNER JOIN  VoertuigInstructeur AS VOIN
+                
+                ON          VOIN.VoertuigId = VOER.Id
+                
+                WHERE       VOIN.InstructeurId = $Id
+                
+                ORDER BY    TYVO.RijbewijsCategorie DESC";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getInstructeurById($Id)
+    {
+        $sql = "SELECT Voornaam
+                      ,Tussenvoegsel
+                      ,Achternaam
+                      ,DatumInDienst
+                      ,AantalSterren
+                FROM  Instructeur
+                WHERE Id = $Id";
+
+        $this->db->query($sql);
+
+        return $this->db->single();
+    }
+    // sql statment om het aantal te laten zien met behulp van count
+    public function getAantalInstructeurs($Id)
+    {
+        $sql = "SELECT COUNT(*) as aantal 
+                FROM Instructeurs
+                WHERE Id = $Id";
+        $this->db->query($sql);
+
+        return $this->db->aantalInstructeurs();
+    }
+}
